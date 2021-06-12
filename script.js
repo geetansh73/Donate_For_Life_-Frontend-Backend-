3780bd7c761ef55976949088a89a96c1bdf8c647
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-06-12 11:00:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function donate()
+{
+location.href="https://donationpage.geetanshg.repl.co/";
+}
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
